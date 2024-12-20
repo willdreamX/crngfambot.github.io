63d5234b1921ec8a7bbcf4a721a403ce0738fde2
Willdream <156038343+willdreamX@users.noreply.github.com>
2024-12-20 17:48:39 +0700

    updated

--- main.js
+++ main.js
@@ -1,7 +1,44 @@
 let tg = window.Telegram.WebApp;
 let user = tg.initDataUnsafe;
-
 tg.expand()
-// let ui_user = document.getElementById('user');
 
-// ui_user.innerHTML = `-> ${user.id}`
+window.onload = function(){
+    let preloader = document.getElementById('page-preloader');
+    
+    setTimeout(() => {
+        preloader.classList.toggle('load', true);
+    }, 1800);
+    setTimeout(() => {
+        preloader.style.display = 'none';
+    }, 2800);
+};
+
+let navs = document.getElementsByClassName('navbar__item');
+
+function ui_nav_action(event) {
+    let nav = document.querySelectorAll('.navbar__item');
+    let screens = document.querySelectorAll('.screen');
+    let target_screen = document.getElementById(event.currentTarget.getAttribute('data-link'));
+
+    console.log(target_screen)
+
+
+    for (let i = 0; i < screens.length; i++) {
+        if (screens[i].id !== target_screen.id) {
+            screens[i].classList.toggle('active', false);
+        }
+    }
+    for (let i = 0; i < nav.length; i++) {
+        if (nav[i].id !== event.target.id) {
+            nav[i].classList.toggle('active', false);
+        }
+    }
+    event.currentTarget.classList.toggle('active', true);
+    target_screen.classList.toggle('active', true)
+}
+
+for (let i = navs.length - 1; i >= 0; i--) {
+    navs[i].onclick = function(event) {
+        ui_nav_action(event);
+    }
+}
